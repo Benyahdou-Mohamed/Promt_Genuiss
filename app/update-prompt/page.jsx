28981f1bd7901914c183c047fa8b764e1904d793
benyahdou-mohamed <benyahdou.moh29@hotmail.com>
2024-04-05 00:03:46 +0100

    f

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,131 +1,131 @@
-"use client"
-import { useEffect, useState, Suspense } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@/components/Form";
+// "use client"
+// import { useEffect, useState, Suspense } from "react";
+// import { useSession } from "next-auth/react";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import Form from "@/components/Form";
 
-const EditPrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-      const [post, setPost] = useState({ prompt: "", tag: "" });
-      const router = useRouter();
-  function Search() {
+// const EditPrompt = () => {
+//   const [submitting, setSubmitting] = useState(false);
+//       const [post, setPost] = useState({ prompt: "", tag: "" });
+//       const router = useRouter();
+//   function Search() {
 
-       // Custom hook to fetch promptId
-       const searchParams =useSearchParams()
-       const promptId=searchParams.get('id')
-      const { data: session } = useSession();
+//        // Custom hook to fetch promptId
+//        const searchParams =useSearchParams()
+//        const promptId=searchParams.get('id')
+//       const { data: session } = useSession();
     
-      useEffect(() => {
-        const getPromptDetails = async () => {
-          try {
-            const response = await fetch(`/api/prompt/${promptId}`);
-            if (response.ok) {
-              const data = await response.json();
-              setPost({
-                prompt: data.prompt,
-                tag: data.tag,
-              });
-            } else {
-              console.error("Failed to fetch prompt details");
-            }
-          } catch (error) {
-            console.error("Error fetching prompt details:", error);
-          }
-        };
+//       useEffect(() => {
+//         const getPromptDetails = async () => {
+//           try {
+//             const response = await fetch(`/api/prompt/${promptId}`);
+//             if (response.ok) {
+//               const data = await response.json();
+//               setPost({
+//                 prompt: data.prompt,
+//                 tag: data.tag,
+//               });
+//             } else {
+//               console.error("Failed to fetch prompt details");
+//             }
+//           } catch (error) {
+//             console.error("Error fetching prompt details:", error);
+//           }
+//         };
     
-        if (promptId) {
-          getPromptDetails();
-        }
-      }, [promptId]);
+//         if (promptId) {
+//           getPromptDetails();
+//         }
+//       }, [promptId]);
     
-      const updatePrompt = async (e) => {
-        e.preventDefault();
-        setSubmitting(true);
-        if (!promptId) return alert("Prompt ID not found");
-        try {
-          const response = await fetch(`api/prompt/${promptId}`, {
-            method: "PATCH",
-            body: JSON.stringify({
-              prompt: post.prompt,
-              tag: post.tag,
-            }),
-          });
-          if (response.ok) {
-            router.push("/");
-          } else {
-            console.error("Failed to update prompt");
-          }
-        } catch (error) {
-          console.error("Error updating prompt:", error);
-        } finally {
-          setSubmitting(false);
-        }
-      };
-    }
-     // Custom hook to fetch promptId
+//       const updatePrompt = async (e) => {
+//         e.preventDefault();
+//         setSubmitting(true);
+//         if (!promptId) return alert("Prompt ID not found");
+//         try {
+//           const response = await fetch(`api/prompt/${promptId}`, {
+//             method: "PATCH",
+//             body: JSON.stringify({
+//               prompt: post.prompt,
+//               tag: post.tag,
+//             }),
+//           });
+//           if (response.ok) {
+//             router.push("/");
+//           } else {
+//             console.error("Failed to update prompt");
+//           }
+//         } catch (error) {
+//           console.error("Error updating prompt:", error);
+//         } finally {
+//           setSubmitting(false);
+//         }
+//       };
+//     }
+//      // Custom hook to fetch promptId
     
-    const { data: session } = useSession();
+//     const { data: session } = useSession();
   
-    useEffect(() => {
-      const getPromptDetails = async () => {
-        try {
-          const response = await fetch(`/api/prompt/${promptId}`);
-          if (response.ok) {
-            const data = await response.json();
-            setPost({
-              prompt: data.prompt,
-              tag: data.tag,
-            });
-          } else {
-            console.error("Failed to fetch prompt details");
-          }
-        } catch (error) {
-          console.error("Error fetching prompt details:", error);
-        }
-      };
+//     useEffect(() => {
+//       const getPromptDetails = async () => {
+//         try {
+//           const response = await fetch(`/api/prompt/${promptId}`);
+//           if (response.ok) {
+//             const data = await response.json();
+//             setPost({
+//               prompt: data.prompt,
+//               tag: data.tag,
+//             });
+//           } else {
+//             console.error("Failed to fetch prompt details");
+//           }
+//         } catch (error) {
+//           console.error("Error fetching prompt details:", error);
+//         }
+//       };
   
-      if (promptId) {
-        getPromptDetails();
-      }
-    }, [promptId]);
+//       if (promptId) {
+//         getPromptDetails();
+//       }
+//     }, [promptId]);
   
-    const updatePrompt = async (e) => {
-      e.preventDefault();
-      setSubmitting(true);
-      if (!promptId) return alert("Prompt ID not found");
-      try {
-        const response = await fetch(`api/prompt/${promptId}`, {
-          method: "PATCH",
-          body: JSON.stringify({
-            prompt: post.prompt,
-            tag: post.tag,
-          }),
-        });
-        if (response.ok) {
-          router.push("/");
-        } else {
-          console.error("Failed to update prompt");
-        }
-      } catch (error) {
-        console.error("Error updating prompt:", error);
-      } finally {
-        setSubmitting(false);
-      }
+//     const updatePrompt = async (e) => {
+//       e.preventDefault();
+//       setSubmitting(true);
+//       if (!promptId) return alert("Prompt ID not found");
+//       try {
+//         const response = await fetch(`api/prompt/${promptId}`, {
+//           method: "PATCH",
+//           body: JSON.stringify({
+//             prompt: post.prompt,
+//             tag: post.tag,
+//           }),
+//         });
+//         if (response.ok) {
+//           router.push("/");
+//         } else {
+//           console.error("Failed to update prompt");
+//         }
+//       } catch (error) {
+//         console.error("Error updating prompt:", error);
+//       } finally {
+//         setSubmitting(false);
+//       }
    
-  }
+//   }
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Search />
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-    </Suspense>
-  );
-};
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Search />
+//       <Form
+//         type="Edit"
+//         post={post}
+//         setPost={setPost}
+//         submitting={submitting}
+//         handleSubmit={updatePrompt}
+//       />
+//     </Suspense>
+//   );
+// };
 
-export default EditPrompt;
+// export default EditPrompt;
